c34b599333939fa6ae2847e210b9e6a7ee9d0bbf
João Pedro Melo <jpmelodasilva309@gmail.com>
2025-09-07 22:46:46 -0300

    criando classe para os animais

---
+++ src/animais.js
@@ -0,0 +1,20 @@
+class Animal {
+  constructor(nome, tipo, brinquedosFavoritos) {
+    this.nome = nome;
+    this.tipo = tipo;
+    this.brinquedosFavoritos = brinquedosFavoritos;
+  }
+}
+
+const ANIMAIS = [
+  new Animal("Rex", "cão", ["RATO", "BOLA"]),
+  new Animal("Mimi", "gato", ["BOLA", "LASER"]),
+  new Animal("Fofo", "gato", ["BOLA", "RATO", "LASER"]),
+  new Animal("Zero", "gato", ["RATO", "BOLA"]),
+  new Animal("Bola", "cão", ["CAIXA", "NOVELO"]),
+  new Animal("Bebe", "cão", ["LASER", "RATO", "BOLA"]),
+  new Animal("Loco", "jabuti", ["SKATE", "RATO"])
+];
+
+
+export { Animal, ANIMAIS };
