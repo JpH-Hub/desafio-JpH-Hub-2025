92e001d66bcde80fc0642e491777eb1e3666dbfd
João Pedro Melo <jpmelodasilva309@gmail.com>
2025-09-09 20:53:03 -0300

    alguns testes unitarios

--- src/abrigo-animais.test.js
+++ src/abrigo-animais.test.js
@@ -28,4 +28,58 @@ describe('Abrigo de Animais', () => {
       expect(resultado.lista.length).toBe(4);
       expect(resultado.erro).toBeFalsy();
   });
+
+  test('A pessoa2 deve adotar o animal Loco', () => {
+    const resultado = new AbrigoAnimais().encontraPessoas('BOLA,LASER',
+      'BOLA,SKATE,RATO,LASER', 'Fofo,Rex,Bola,Loco');
+
+      expect(resultado.lista[0]).toBe('Bola - abrigo');
+      expect(resultado.lista[1]).toBe('Fofo - pessoa 2');
+      expect(resultado.lista[2]).toBe('Loco - pessoa 2');
+      expect(resultado.lista[3]).toBe('Rex - abrigo');
+      expect(resultado.lista.length).toBe(4);
+      expect(resultado.erro).toBeFalsy();
+  });
+
+  test('A pessoa1 deve adotar o animal Loco', () => {
+    const resultado = new AbrigoAnimais().encontraPessoas('BOLA,SKATE,RATO,LASER',
+      'BOLA,LASER', 'Fofo,Rex,Bola,Loco');
+
+      expect(resultado.lista[0]).toBe('Bola - abrigo');
+      expect(resultado.lista[1]).toBe('Fofo - pessoa 1');
+      expect(resultado.lista[2]).toBe('Loco - pessoa 1');
+      expect(resultado.lista[3]).toBe('Rex - abrigo');
+      expect(resultado.lista.length).toBe(4);
+      expect(resultado.erro).toBeFalsy();
+  });
+
+  test('Deve enviar ambos animais para o abrigo pois ambas tem condição de adoção', () => {
+    const resultado = new AbrigoAnimais().encontraPessoas('RATO,BOLA,LASER',
+      'RATO,BOLA', 'Rex');
+      expect(resultado.lista[0]).toBe('Rex - abrigo');
+      expect(resultado.lista.length).toBe(1);
+      expect(resultado.erro).toBeFalsy();
+  });
+
+
+
+  test('Deve enviar o animal Loco para o abrigo pois ele nao tem compania', () => {
+    const resultado = new AbrigoAnimais().encontraPessoas('BOLA,LASER',
+      'BOLA,SKATE,RATO,LASER', 'Loco,Fofo,Rex,Bola');
+
+      expect(resultado.lista[0]).toBe('Bola - abrigo');
+      expect(resultado.lista[1]).toBe('Fofo - pessoa 2');
+      expect(resultado.lista[2]).toBe('Loco - abrigo');
+      expect(resultado.lista[3]).toBe('Rex - abrigo');
+      expect(resultado.lista.length).toBe(4);
+      expect(resultado.erro).toBeFalsy();
+  });
+
+
+  test('Deve dar erro ao botar animais duplicados', () => {
+    const resultado = new AbrigoAnimais().encontraPessoas('BOLA,LASER',
+      'BOLA,NOVELO,RATO,LASER', 'Mimi, Fofo, Rex, Rex');
+
+      expect(resultado.erro).toBe('Animal inválido');
+  });
 });
